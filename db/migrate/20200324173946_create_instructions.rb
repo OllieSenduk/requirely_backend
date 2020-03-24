3a9b94328569d65f19ba6ae49e87f7039036b1e4
Inou Ridder <inouridder@gmail.com>
2020-03-24 18:43:52 +0100

    ft: add instructions factory

---
+++ db/migrate/20200324173946_create_instructions.rb
@@ -0,0 +1,12 @@
+class CreateInstructions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :instructions do |t|
+      t.references :structure, null: false, foreign_key: true
+      t.references :instruction, null: false, foreign_key: true
+      t.string :name
+      t.string :content
+
+      t.timestamps
+    end
+  end
+end
